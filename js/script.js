c74335c6319970c9632e947b11f2c908ebf6fbce
Rahman Mohammad Abdur <47378897+abdurrahman16@users.noreply.github.com>
2025-12-25 22:32:05 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var a=prompt('Enter');
+switch (a){
+    case "a" : 
+    document.write('I am A'); break;
+    case "b":
+        document.write(' I am B'); break;
+    default:
+    
+     document.write("No options")
+
+ 
+}
